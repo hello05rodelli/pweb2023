23459dff37b4935bc7b668d9cb23523979825903
hello05rodelli <127855203+hello05rodelli@users.noreply.github.com>
2023-10-20 23:36:58 -0300

    Add files via upload

---
+++ api/src/main/resources/static/medico.js
@@ -0,0 +1,15 @@
+{
+	"id": null,
+	"nome":"Hudson de Paula Romualrio",
+	"email": "hudson.romualdo@ifgoiano.edu.br",
+	"crm": "516126",
+	"especialidade": "ORTOPEDIA",
+	"endereco":{
+		"logradouro": "Rua Wilson Reis",
+		"bairro": "Crinstina II",
+		"complemento": "Quadra 6, lote 15",
+		"cep":"154365425",
+		"cidade": "Trindade",
+		"estado": "GO"
+	} 
+}
